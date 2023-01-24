ed786333de1adf132d6116dd3b6f3b847193c1c2
abdullah-taha5 <75340111+abdullah-taha5@users.noreply.github.com>
2023-01-24 19:53:30 +0200

    new comm

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const helmet = require("helmet");
+const bodyParser = require("body-parser");
+;
+
 require("dotenv").config();
 
 
@@ -10,8 +13,11 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 app.use(helmet());
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 
 // Connection To DB
+mongoose.set('strictQuery', true);
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("Connected To Data Base"))
