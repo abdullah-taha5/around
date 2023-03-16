ca8385b259471cf058ccea38863d2cc1605757e8
abdullah-taha5 <75340111+abdullah-taha5@users.noreply.github.com>
2023-03-16 15:10:43 +0200

    Update ordersController.js

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -3,7 +3,8 @@ const jwt = require("jsonwebtoken");
 const cloudscraper = require("cloudscraper");
 const { NotificationsDriver, NotificationAdmin, NotificationsClient } = require("../models/Notifications");
 const Pusher = require("pusher");
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-extra");
+const hidden = require('puppeteer-extra-plugin-stealth')
 
 /**
  * @desc Create New Order
