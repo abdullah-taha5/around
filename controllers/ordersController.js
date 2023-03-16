260bb3456c236619e044900a5ef0b335405ef42f
abdullah-taha5 <75340111+abdullah-taha5@users.noreply.github.com>
2023-03-16 15:43:05 +0200

    Update ordersController.js

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -3,8 +3,8 @@ const jwt = require("jsonwebtoken");
 const cloudscraper = require("cloudscraper");
 const { NotificationsDriver, NotificationAdmin, NotificationsClient } = require("../models/Notifications");
 const Pusher = require("pusher");
-const puppeteer = require("puppeteer-extra");
-const hidden = require('puppeteer-extra-plugin-stealth')
+const puppeteer = require("puppeteer");
+
 
 /**
  * @desc Create New Order
@@ -110,14 +110,17 @@ const getSingleOrder = async (req, res) => {
  */
 const searchOrder = async (req, res) => {
   (async () => {
-    puppeteer.use(hidden())
-    const browser = await puppeteer.launch({      args: [
+    
+    const browser = await puppeteer.launch({    args: [
       "--disable-setuid-sandbox",
       "--no-sandbox",
       "--single-process",
       "--no-zygote",
     ],
-    executablePath: puppeteer.executablePath(),});
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),});
     
     const page = await browser.newPage();
     await page.goto("https://itp.gov.iq/carSearch.php");
